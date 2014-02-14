7a98d197df2247d42fd6af64f0d993a168f3204e
steve <stevedanko1@gmail.com>
2014-02-13 21:00:03 -0500

    first commit working program

---
+++ boiled_eggs.rb
@@ -0,0 +1,15 @@
+def cooking_time(eggs)
+  number_of_batches = eggs.to_f/8
+  if number_of_batches == 0
+    return 0
+  elsif number_of_batches.to_s.include?(".")
+    split_batches = number_of_batches.to_s.split(".")
+    if split_batches[1].to_i >= 0 
+      number_of_batches = split_batches[0].to_i + 1
+      time_to_cook = number_of_batches * 5
+    end
+  else
+    time_to_cook = number_of_batches * 5
+  end 
+  time_to_cook 
+end
