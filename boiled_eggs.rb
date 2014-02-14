37d78b5ae49ba482c715b401fba62301d0f71321
steve <stevedanko1@gmail.com>
2014-02-13 21:19:14 -0500

    fixed bug so all cases work now

--- boiled_eggs.rb
+++ boiled_eggs.rb
@@ -1,14 +1,20 @@
+require 'pry'
+
 def cooking_time(eggs)
   number_of_batches = eggs.to_f/8
   if number_of_batches == 0
     return 0
   elsif number_of_batches.to_s.include?(".")
     split_batches = number_of_batches.to_s.split(".")
-    if split_batches[1].to_i >= 0 
+    if split_batches[1].to_i > 0
       number_of_batches = split_batches[0].to_i + 1
       time_to_cook = number_of_batches * 5
+    else
+      time_to_cook = number_of_batches.to_i * 5
     end
   else
-    time_to_cook = number_of_batches * 5
-  end 
+    time_to_cook = number_of_batches.to_i * 5
+  end
 end
+
+puts cooking_time(8)
