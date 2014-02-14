7fabd1a0c3e21aeda76ccbcc92455787683d04b6
steve <stevedanko1@gmail.com>
2014-02-13 21:43:21 -0500

    refactored

--- boiled_eggs.rb
+++ boiled_eggs.rb
@@ -1,16 +1,8 @@
 def cooking_time(eggs)
-  number_of_batches = eggs.to_f/8
-  if number_of_batches == 0
-    return 0
-  elsif number_of_batches.to_s.include?(".")
-    split_batches = number_of_batches.to_s.split(".")
-    if split_batches[1].to_i > 0
-      number_of_batches = split_batches[0].to_i + 1
-      time_to_cook = number_of_batches * 5
-    else
-      time_to_cook = number_of_batches.to_i * 5
-    end
+  time_to_cook = eggs.to_f/8
+  if time_to_cook >= 0
+    time_to_cook = time_to_cook.ceil * 5
   else
-    time_to_cook = number_of_batches.to_i * 5
+    time_to_cook = 0
   end
 end
